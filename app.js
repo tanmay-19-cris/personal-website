e2682709d01628a9bf7033df48c1ac16c57b8297
tanmay <tanmayshet2010@gmail.com>
2026-07-26 23:08:34 +0530

    adjusted delay in intro

--- app.js
+++ app.js
@@ -2,29 +2,29 @@ let intro = document.querySelector('.intro');
 let logo = document.querySelector('.logo-header');
 let logoSpan = document.querySelectorAll('.logo');
 
-window.addEventListener('DOMContentLoaded', ()=>{
+window.addEventListener('DOMContentLoaded', () => {
 
-    setTimeout(()=>{
+    setTimeout(() => {
 
-        logoSpan.forEach((span, idx)=>{
-            setTimeout(()=>{
+        logoSpan.forEach((span, idx) => {
+            setTimeout(() => {
                 span.classList.add('active');
-            }, (idx + 1) * 400)
+            }, (idx + 1) * 300)
         });
 
-        setTimeout(()=>{
-            logoSpan.forEach((span, idx)=>{
+        setTimeout(() => {
+            logoSpan.forEach((span, idx) => {
 
-                setTimeout(()=>{
+                setTimeout(() => {
                     span.classList.remove('active');
                     span.classList.add('fade');
                 }, (idx + 1) * 50)
             })
-        }, 2000);
+        }, 4000);
 
-        setTimeout(()=>{
+        setTimeout(() => {
             intro.style.top = '-100vh';
-        }, 2300)
+        }, 1200)
 
     }, 1200)
 
